e410f386137994c44095e65c20250ec05b64ce25
George Lerner <lernerconsult@gmail.com>
2024-11-13 09:33:04 -0700

    More GitHub stuff, Composer, testing

--- tests/TestHelpers.php
+++ tests/TestHelpers.php
@@ -12,35 +12,58 @@ namespace GLColorPalette\Tests;
 
 class TestHelpers {
     /**
-     * Create a test palette
+     * Create a mock provider with test credentials
      *
-     * @param string $name Palette name
-     * @param array  $colors Array of colors
-     * @return int Palette ID
+     * @param string $provider_class The provider class name
+     * @param array  $credentials    Test credentials
+     * @return object
      */
-    public static function create_test_palette(string $name, array $colors): int {
-        global $wpdb;
+    public static function create_mock_provider($provider_class, $credentials = []) {
+        $default_credentials = [
+            'api_key' => 'test_key',
+            'model' => 'test_model',
+            'resource_name' => 'test_resource',
+            'deployment_id' => 'test_deployment'
+        ];
 
-        $wpdb->insert(
-            $wpdb->prefix . 'gl_color_palettes',
-            [
-                'name' => $name,
-                'colors' => json_encode($colors),
-                'created_at' => current_time('mysql'),
-            ],
-            ['%s', '%s', '%s']
-        );
-
-        return $wpdb->insert_id;
+        return new $provider_class(array_merge($default_credentials, $credentials));
     }
 
     /**
-     * Clean up test palettes
+     * Assert that a color is a valid hex code
      *
+     * @param string $color The color to test
      * @return void
      */
-    public static function cleanup_test_palettes(): void {
-        global $wpdb;
-        $wpdb->query("TRUNCATE TABLE {$wpdb->prefix}gl_color_palettes");
+    public static function assertValidHexColor($color) {
+        $pattern = '/#[a-fA-F0-9]{6}/';
+        if (!preg_match($pattern, $color)) {
+            throw new \PHPUnit\Framework\AssertionFailedError(
+                sprintf('Color "%s" is not a valid hex code', $color)
+            );
+        }
+    }
+
+    /**
+     * Assert that an array contains valid hex colors
+     *
+     * @param array $colors Array of colors to test
+     * @param int   $expected_count Expected number of colors
+     * @return void
+     */
+    public static function assertValidColorPalette($colors, $expected_count = 5) {
+        if (!is_array($colors)) {
+            throw new \PHPUnit\Framework\AssertionFailedError('Colors must be an array');
+        }
+
+        if (count($colors) !== $expected_count) {
+            throw new \PHPUnit\Framework\AssertionFailedError(
+                sprintf('Expected %d colors, got %d', $expected_count, count($colors))
+            );
+        }
+
+        foreach ($colors as $color) {
+            self::assertValidHexColor($color);
+        }
     }
 } 
